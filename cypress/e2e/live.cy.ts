558932c9fd74a5ca2dca4b5db6d465f1bf59f0ef
Lasitha Wijenayake <lawi@trackman.com>
2025-06-17 14:45:05 +0200

    Enhance `CliCy` with expanded Cypress DSL commands, detailed REPL improvements, eBay search test integration, and updated history functionality.

--- cypress/e2e/live.cy.ts
+++ cypress/e2e/live.cy.ts
@@ -1,10 +1,6 @@
 
 describe('Live Test', () => {
   it('runs REPL steps', () => {
-    cy.visit("https://www.ebay.com")
-    cy.get(".gh-search-button__label").should("be.visible")
-    cy.wait(3000)
-    cy.get("#gh-ac").type("Apple iphone")
-    cy.get(".gh-search-button__label").click()
+    
   });
 });
